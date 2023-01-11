7ce4eac3d9073abd62884203ffc79766b8edb21c
추준성 <55497825+031wnstjd@users.noreply.github.com>
2023-01-11 23:08:44 +0900

    Update day2_05_프린터큐.cpp

--- algorithm/day2/day2_05_프린터큐.cpp
+++ algorithm/day2/day2_05_프린터큐.cpp
@@ -55,9 +55,9 @@ void Solve() {
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 	
 	int TC;
 	cin >> TC;
